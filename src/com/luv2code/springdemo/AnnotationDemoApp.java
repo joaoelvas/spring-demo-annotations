4456e4abdd30b32b2ea40ad4e740c323e131dd78
João Elvas <joao.elvas@gmail.com>
2018-10-13 19:00:53 +0100

    Commit

---
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,24 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+	
+		// read spring config file
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// get bean from spring container
+		Coach thatSillyCoach = context.getBean("tennisCoach", Coach.class);
+		
+		// call method on the bean
+		System.out.println(thatSillyCoach.getDailyWorkout());
+		
+		// close the context
+		context.close();
+
+	}
+
+}
